a16a00a444db71ef0e39d2be49858774b73402bd
Shabonkerz <Shabonkerz@gmail.com>
2016-04-21 14:49:19 -0500

    bug fix: rollback in proper order(reverse)

--- src/transactionManager.js
+++ src/transactionManager.js
@@ -12,13 +12,21 @@ export default class TransactionManager {
 		this.store = store;
 		this.transactions = [];
 
+		// Used to prevent the rollback from adding more commands to undo while
+		// undoing commands.
+		this.ignoreEvents = false;
+
 		this.store
 			.on('set', (key, previous, current) => {
+
 				const transaction = this.transactions[this.transactions.length - 1];
 
+				// We don't need to do anything if there is no top-level
+				// transaction.
 				if (!transaction) {
 					return;
 				}
+
 
 				transaction.undoCommands.push( () => {
 					if (previous) {
@@ -30,6 +38,7 @@ export default class TransactionManager {
 				});
 			})
 			.on('unset', (key, value) => {
+
 				const transaction = this.transactions[this.transactions.length - 1];
 
 				if (!transaction) {
@@ -47,23 +56,36 @@ export default class TransactionManager {
 	}
 
 	rollback () {
+
+		// Ignore if there are no transactions present.
 		if (!this.transactions.length) {
 			return;
 		}
 
+		// Only rollback the top-level transaction.
 		const currentTransaction = this.transactions[this.transactions.length - 1];
 
-		_.forEach(currentTransaction.undoCommands, (command) => {
+		// We don't want more commands pushed onto the undoCommands stack while
+		// we're executing them. _.each and _.eachRight already ignore
+		// modifications to the array while iterating over it, but it doesn't
+		// prevent items from being added.
+		this.ignoreEvents = true;
+		_.eachRight(currentTransaction.undoCommands, (command) => {
 			command();
 		});
+		this.ignoreEvents = false;
 
 		this.transactions.pop();
 	}
 
 	commit () {
+		// Ignore if no transactions are present.
 		if (!this.transactions.length) {
 			return;
 		}
+
+		// Changes are already in the store, so essentially we just need to
+		// discard the transaction and its undo commands.
 		this.transactions.pop();
 	}
 }
